6650ea99a0b0f882fc9ebda253b102a08b125e8e
Filippo Pilutti <filippo.pilutti@studio.unibo.it>
2020-11-22 12:08:36 +0100

    Complete third exercise lambda ex03

--- src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
+++ src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
@@ -1,12 +1,15 @@
 package it.unibo.oop.lab.lambda.ex03;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.util.function.Function;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -35,8 +38,21 @@ public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
 
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
+
     private enum Command {
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        LOWERCASE("Lowercase", String::toLowerCase),
+        COUNT("Count chars", s -> Integer.toString(s.length())),
+        COUNT_LINES("Count lines", s -> Long.toString(s.chars().filter(e -> e == '\n').count() + 1)),
+        WORDS("Sort words in aplhabetical order", s -> 
+             Arrays.stream(s.split(ANY_NON_WORD)).sorted().collect(Collectors.joining("\n"))),
+        WORDCOUNT("Count words", s -> 
+            Arrays.stream(s.split(ANY_NON_WORD))
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .entrySet().stream()
+            .map(e -> e.getKey() + " -> " + e.getValue())
+            .collect(Collectors.joining("\n")));
 
         private final String commandName;
         private final Function<String, String> fun;
